3cff715d1b8a740c21c6b344c96d3cdee32c03ee
huhongwei <hongwei.hu@foxmail.com>
2023-12-20 08:48:38 +0800

    新增背景音乐，添加TileMap

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float m_moveSpeed;
+    public bool m_isGameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_isGameover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!m_isGameover)
+        {
+            transform.position += new Vector3(Time.fixedDeltaTime * m_moveSpeed, 0, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Game over");
+            m_isGameover = true;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,35 +5,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private bool _isJump = false;
-    Animator _animator;
-    Rigidbody2D _rigidBody;
+    public float m_jumpSpeed;
+    public float m_moveSpeed;
+    private bool m_isJump = false;
+    private bool m_isRunning = true;
+    Animator m_animator;
+    Rigidbody2D m_rigidBody;
 
     private void Awake()
     {
-        _animator = GetComponent<Animator>();
-        _rigidBody = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        m_rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool jump = Input.GetKeyDown(KeyCode.Space);
-        if (jump && !_isJump)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            _isJump = true;
-            _animator.SetBool("Jump", true);
-            _rigidBody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+            m_isJump = true;
         }
     }
 
     private void FixedUpdate()
     {
+        if (m_isJump)
+        {
+            m_animator.SetBool("Jump", true);
+            m_rigidBody.AddForce(Vector2.up * m_jumpSpeed, ForceMode2D.Impulse);
+            m_isJump = false;
+        }
+        m_rigidBody.position += new Vector2(m_moveSpeed * Time.fixedDeltaTime, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        _isJump = false;
-        _animator.SetBool("Jump", false);
+        m_isJump = false;
+        m_animator.SetBool("Jump", false);
     }
 }
